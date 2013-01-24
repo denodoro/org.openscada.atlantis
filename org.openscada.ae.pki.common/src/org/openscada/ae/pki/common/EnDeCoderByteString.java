e1daaf1fef40cd61269402721327938bcaff38e2
Dennie <denodoro@hotmail.de>
2013-01-24 11:24:54 +0100

    Encodieren und Decodieren mit base64 funktioniert jetzt auch in der Testklasse, da gab es noch einen Fehler

--- org.openscada.ae.pki.common/src/org/openscada/ae/pki/common/EnDeCoderByteString.java
+++ org.openscada.ae.pki.common/src/org/openscada/ae/pki/common/EnDeCoderByteString.java
@@ -1,5 +1,9 @@
 package org.openscada.ae.pki.common;
 
+import java.io.IOException;
+
+import org.openscada.utils.codec.Base64;
+
 //import org.apache.commons.codec.binary.StringUtils;
 
 public class EnDeCoderByteString {
@@ -15,13 +19,17 @@ public class EnDeCoderByteString {
     }
 
 	public String encodeBytearrayToString(byte[] byteArray) {
-		return new String(byteArray);
+		return Base64.encodeBytes( byteArray ); 
 		//return StringUtils.newStringIso8859_1(byteArray);
 	}
 
 	public byte[] decodeStringToBytearray(String string) {
-		return string.getBytes();
-		//return StringUtils.getBytesIso8859_1(string);
+		try {
+			return Base64.decode(string);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 
 }
